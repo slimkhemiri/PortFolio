20d4971b61a97051f4e708570b2d5e6c4cc240a3
slimkhemiri <khemiri.medslim1@gmail.com>
2021-11-21 20:52:13 +0000

    First steps

---
+++ src/components/About/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <Image
+            src="https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+          <div className="AboutBio">
+            Hello! My name is <strong>Khemiri Slim</strong> I’m
+            graduated as a full stack software developer. I worked on respectful
+            amount of projects which helped me to sharpen my skills to a higher
+            and qualified level, And I’m looking forward to be an expert
+            developer with strong hard and soft skills.
+          </div>
+          <div className="AboutBio tagline2">
+            I have become confident using the following technologies.
+          </div>
+          <Technologies>
+            {stackList.map((stack, index) => (
+              <Tech key={index} className="tech">
+                <TechImg src={stack.img} alt={stack.name} />
+                <TechName>{stack.name}</TechName>
+              </Tech>
+            ))}
+          </Technologies>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,120 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_1.png",
+    title: "Fit-Hub",
+    description:
+      "Mobile App for fitness and health where you can find the nearest Gyms and healthy food restaurants, you can also book for a private coach as a user, join some events and follow the progress of your health. ",
+    tech_stack: "React Native, Typescript, Tailwind Css, Nest Js, PostgreSQL, Heroku, Next Js",
+    github_url: "https://github.com/slimkhemiri/Fit-Hub",
+    demo_url: "https://your_project_demo_link",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_2.png",
+    title: "O-Teach",
+    description:
+      "An online Teaching platform where you can study and take lectures and sections as a user and as an admin you can create lectures and groupsto join",
+    tech_stack: "React Js, Bootstrap, Node Js, Express Js, MongoDB",
+    github_url: "https://github.com/slimkhemiri/oTeach",
+    demo_url: "https://your_project_demo_link",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/portfolio.png",
+    title: "Portfolio",
+    description: "Ofcourse i made my own personal portfolio website.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link/",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_3.png",
+    title: "RMDB",
+    description:
+      "A Website where one can discover the popular movies, the best known and most recent in the form of trailers.",
+    tech_stack: "Angular, Node Js, Express Js, MongoDB",
+    github_url: "https://github.com/slimkhemiri/RMDB",
+    demo_url: "https://your_project_demo_link",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/512px-Typescript_logo_2020.svg.png",
+    name: "TypeScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://www.kindpng.com/picc/m/17-176451_atom-react-native-logo-svg-hd-png-download.png",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://www.whitelabeldevelopers.tech/upload/glossary/492/headimg.png",
+    name: "Tailwind Css",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/sass.svg",
+    name: "Scss",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png",
+    name: "Angular",
+  },
+  {
+    img: "https://seeklogo.com/images/N/nestjs-logo-09342F76C0-seeklogo.com.png",
+    name: "NestJs",
+  },
+  {
+    img: "https://media.istockphoto.com/vectors/nodejs-vector-logo-backend-programming-in-javascript-server-vector-id1195857274?k=20&m=1195857274&s=170667a&w=0&h=k8oHsv3ehrvhviozLlvhEjGHweiHU7hbBv7bHEkgUqc=",
+    name: "NodeJs",
+  },
+  {
+    img: "https://www.resourcifi.com/wp-content/themes/resourcifi-child/img/express-min.png",
+    name: "ExpressJs",
+  },
+  {
+    img: "https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Emblem.jpg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://download.logo.wine/logo/MySQL/MySQL-Logo.wine.png",
+    name: "MySQL",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png",
+    name: "PostgreSQL",
+  },
+  {
+    img: "https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png",
+    name: "Next Js",
+  },
+];
